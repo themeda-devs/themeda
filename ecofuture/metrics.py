2171dce106cb37d3c21b673354d0cc823c35a435
rturnbull <robert.turnbull@unimelb.edu.au>
2023-05-25 15:05:49 +1000

    :zap: fixing accuracy metric

--- ecofuture/metrics.py
+++ ecofuture/metrics.py
@@ -1,6 +1,12 @@
 import torch
 
-def accuracy(prediction, target):
-    prediction = torch.argmax(prediction, dim=2)
-    correct = prediction == target
-    return correct.float().mean()
+def accuracy(predictions, *targets):
+    if not isinstance(predictions, tuple):
+        predictions = (predictions,)
+
+    assert len(predictions) == len(targets)
+
+    for prediction, target in zip(predictions, targets):
+        prediction = torch.argmax(prediction, dim=2) # only for categorical
+        correct = prediction == target
+        return correct.float().mean() # hack
