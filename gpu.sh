9479794973f571a778502d73f251c292470308fb
Robert Turnbull <rturnbull@rdl-ns4a8z.desktop.cloud.unimelb.edu.au>
2023-10-18 16:48:37 +1100

    :tv: pinning torch to version 2.0.0

--- gpu.sh
+++ gpu.sh
@@ -1,1 +1,1 @@
-sinteractive --account=punim1932 --time=20 --ntasks=1 --cpus-per-task 1 -p gpu-a100 --gres=gpu:1
+sinteractive --account=punim1932 --time=20 --ntasks=1 --cpus-per-task 1 -p gpu-a100-short --gres=gpu:1
---
+++ train.sh
@@ -0,0 +1,21 @@
+BATCH=2
+LEARNING_RATE=0.001
+KERNEL=15
+HIDDEN=64
+EMBEDDING=16
+
+TEMPORAL=lstm
+
+RUN_NAME=themeda-demo
+
+poetry run themeda train \
+    --input land_cover --input rain --input tmax --input elevation --input land_use \
+    --input fire_scar_early  --input fire_scar_late \
+    --input soil_ece --input soil_clay --input soil_depth \
+    --output land_cover \
+    --validation-subset 1 --batch-size $BATCH \
+    --learning-rate $LEARNING_RATE --temporal-processor-type $TEMPORAL \
+    --kernel-size $KERNEL --embedding-size $EMBEDDING \
+    --base-dir $THEMEDA_DATA_DIR \
+    --run-name $RUN_NAME --output-dir outputs/$RUN_NAME \
+    # --wandb --wandb-entity punim1932
