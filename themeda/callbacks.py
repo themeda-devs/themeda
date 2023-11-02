:bug: fixing incorrect axis for softmax during inference

themeda/callbacks.py
@@ -23,7 +23,11 @@ class WriteResults(Callback):
     def after_batch(self): 
         land_cover_prediction = self.learn.pred[0]
         land_cover_prediction_final_year = land_cover_prediction[:,-1]
-        land_cover_probabilities = land_cover_prediction_final_year.softmax(dim=2)
+        # after indexing in to the final year, the feature axis should 1 instead of 2
+        feature_axis = 1
+        assert land_cover_prediction_final_year.shape[feature_axis] == 23
+        land_cover_probabilities = land_cover_prediction_final_year.softmax(dim=feature_axis)
+        assert len(land_cover_probabilities.shape) == 4
         batch_size = len(land_cover_probabilities)
         end_point = self.current_index + batch_size
         self.chiplets[self.current_index:end_point, ...] = np.array(land_cover_probabilities.cpu(), dtype=self.chiplets.dtype)