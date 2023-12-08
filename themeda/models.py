9d81702997ca11c77d08a17eb5bc735aa22b39ff
rturnbull <robert.turnbull@unimelb.edu.au>
2023-12-08 12:20:27 +1100

    :zap: adding growth factor as cli option

--- themeda/models.py
+++ themeda/models.py
@@ -98,23 +98,13 @@ class PositionalEncoding(nn.Module):
         return x + self.pe[:x.size(1)].permute(1,0,2)
 
 
-
 def Conv(*args, dim:int, **kwargs):
     if dim == 2:
         return nn.Conv2d(*args, **kwargs)
     if dim == 3:
         return nn.Conv3d(*args, **kwargs)
     raise ValueError(f"dimension {dim} not supported")    
-    
-
-def BatchNorm(*args, dim:int, **kwargs):
-    return nn.Identity()
-    if dim == 2:
-        return nn.BatchNorm2d(*args, **kwargs)
-    if dim == 3:
-        return nn.BatchNorm3d(*args, **kwargs)
-    raise ValueError(f"dimension {dim} not supported")    
-    
+        
 
 def ConvTranspose(*args, dim:int, **kwargs):
     if dim == 2:
@@ -151,26 +141,18 @@ class ResBlock(nn.Module):
 
         if downsample:
             self.conv1 = Conv(in_channels, out_channels, kernel_size=kernel_size, stride=2, padding=padding, padding_mode=padding_mode, dim=dim)
-            self.shortcut = nn.Sequential(
-                Conv(in_channels, out_channels, kernel_size=1, stride=2, padding_mode=padding_mode, dim=dim), 
-                BatchNorm(out_channels, dim=dim)
-            )
+            self.shortcut = Conv(in_channels, out_channels, kernel_size=1, stride=2, padding_mode=padding_mode, dim=dim)
         else:
             self.conv1 = Conv(in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding, padding_mode=padding_mode, dim=dim)
-            self.shortcut = nn.Sequential()
+            self.shortcut = nn.Identity()
 
         self.conv2 = Conv(out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=padding, padding_mode=padding_mode, dim=dim)
-        self.bn1 = BatchNorm(out_channels, dim=dim)
-        self.bn2 = BatchNorm(out_channels, dim=dim)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x: Tensor):
         shortcut = self.shortcut(x)
-        # print('shortcut max', shortcut.max())
-        x = self.relu(self.bn1(self.conv1(x)))
-
-        x = self.relu(self.bn2(self.conv2(x)))
-
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
         x = self.relu(x + shortcut)
 
         return x
@@ -219,276 +201,6 @@ class UpBlock(nn.Module):
         x = self.block1(x)
         # x = self.block2(x)
         return x
-
-
-
-class UNetDecoder(nn.Module):
-    def __init__(
-        self,
-        in_channels:int,
-        initial:int,
-        out_channels:int,
-        kernel_size:int=3,
-        final_upsample_dims:int=16,
-        dropout:float=0.0,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.upblock3 = UpBlock(
-            dim=2, 
-            in_channels=in_channels, 
-            out_channels=in_channels//2, 
-            resblock_kernel_size=kernel_size
-        )
-        self.upblock2 = UpBlock(
-            dim=2, 
-            in_channels=in_channels//2, 
-            out_channels=in_channels//4, 
-            resblock_kernel_size=kernel_size
-        )
-        self.upblock1 = UpBlock(
-            dim=2, 
-            in_channels=in_channels//4, 
-            out_channels=in_channels//8, 
-            resblock_kernel_size=kernel_size
-        )
-        self.upblock0b = UpBlock(
-            dim=2, 
-            in_channels=in_channels//8, 
-            out_channels=in_channels//16, 
-            resblock_kernel_size=kernel_size
-        )
-        self.upblock0a = UpBlock(
-            dim=2, 
-            in_channels=in_channels//16, 
-            out_channels=in_channels//32, 
-            resblock_kernel_size=kernel_size
-        )
-        self.dropout = nn.Dropout(dropout)
-        self.final_layer = Conv(
-            in_channels=final_upsample_dims+initial, 
-            out_channels=out_channels, 
-            kernel_size=1,
-            stride=1,
-            dim=2,
-        )
-        
-    def forward(self, initial, l1, l2, l3, x):
-        x, time_distributed, batch_size, timesteps = time_distributed_combine(x)
-        l1, _, _, _ = time_distributed_combine(l1)
-        l2, _, _, _ = time_distributed_combine(l2)
-        l3, _, _, _ = time_distributed_combine(l3)
-        initial, _, _, _ = time_distributed_combine(initial)
-
-        x = self.dropout(x)
-        l1 = self.dropout(l1)
-        l2 = self.dropout(l2)
-        l3 = self.dropout(l3)
-        initial = self.dropout(initial)
-
-        x = self.upblock3(x,l3)
-        x = self.upblock2(x,l2)
-        x = self.upblock1(x,l1)
-        x = self.upblock0b(x)
-        x = self.upblock0a(x)
-
-        x = torch.cat([initial,x], dim=1)
-
-        x = self.final_layer(x)
-        if time_distributed:
-            # returns samples, timesteps, output_size
-            x = x.contiguous().view( (batch_size, timesteps, -1) + x.shape[2:] )  
-
-        return x
-
-
-class ResnetSpatialEncoder(nn.Module):
-    def __init__(
-        self,
-        in_channels:int,
-        encoder_resent:ResNet|str=ResNet.resnet18,
-        average_channels:bool = True,
-        weights:str = "DEFAULT",
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        encoder_resent = str(encoder_resent)
-
-        assert hasattr(visionmodels, encoder_resent)
-        self.resnet = getattr(visionmodels, encoder_resent)(weights=weights)
-        assert isinstance(self.resnet, visionmodels.resnet.ResNet)
-        self.average_channels = average_channels
-
-        # Modify input channels weights
-        new_weights = self.resnet.conv1.weight.data.sum(dim=1, keepdim=True)/in_channels
-        new_weights = new_weights.repeat( 1, in_channels, 1, 1 )
-        params = {attr:getattr(self.resnet.conv1, attr) for attr in 'out_channels kernel_size stride padding dilation groups padding_mode'.split()}
-        params['bias'] = bool(self.resnet.conv1.bias)
-        params['in_channels'] = in_channels
-
-        self.resnet.conv1 = nn.Conv2d( **params )
-        self.resnet.conv1.data = new_weights
-
-    def forward(self, x):
-        initial = x
-
-
-        x, time_distributed, batch_size, timesteps = time_distributed_combine(x)
-
-        x = self.resnet.conv1(x)
-        x = self.resnet.bn1(x)
-        x = self.resnet.relu(x)
-        x = self.resnet.maxpool(x)
-
-        l1 = x = self.resnet.layer1(x)
-        l2 = x = self.resnet.layer2(x)
-        l3 = x = self.resnet.layer3(x)
-        l4 = x = self.resnet.layer4(x)
-
-        if self.average_channels:
-            x = self.resnet.avgpool(x).squeeze()
-
-        if time_distributed:
-            # returns samples, timesteps, output_size
-            x = x.contiguous().view( (batch_size, timesteps, -1) + x.shape[2:] )  
-            l1 = l1.contiguous().view( (batch_size, timesteps, -1) + l1.shape[2:] )  
-            l2 = l2.contiguous().view( (batch_size, timesteps, -1) + l2.shape[2:] )  
-            l3 = l3.contiguous().view( (batch_size, timesteps, -1) + l3.shape[2:] )  
-            l4 = l4.contiguous().view( (batch_size, timesteps, -1) + l4.shape[2:] )  
-
-        return x, initial, l1, l2, l3, l4
-
-
-class ThemedaModelUNet(nn.Module):
-    def __init__(
-        self,
-        input_types:List[PolyData],
-        output_types:List[PolyData] = None,
-        embedding_size:int=16,        
-        encoder_resent:ResNet|str=ResNet.resnet18,
-        encoder_weights:str="DEFAULT",
-        temporal_processor_type:TemporalProcessorType|str=TemporalProcessorType.LSTM,
-        decoder_type:DecoderType|str=DecoderType.UNET,
-        temporal_layers:int=2,
-        temporal_size:int=512,
-        temporal_bias:bool=True,
-        dropout:float=0.0,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.embedding = PolyEmbedding(
-            input_types=input_types,
-            embedding_size=embedding_size,
-            feature_axis=2,
-        )
-
-        output_types = output_types or input_types
-        self.output_types = output_types
-        out_channels = total_size(output_types)
-        
-        self.spatial_encoder = ResnetSpatialEncoder(
-            in_channels=embedding_size, 
-            encoder_resent=encoder_resent, 
-            weights=encoder_weights,
-        )
-
-        # Set up the temporal processor
-        temporal_dims = 512
-        rnn_kwargs = dict(
-            batch_first=True, 
-            bidirectional=False, 
-            input_size=temporal_dims,
-            num_layers=temporal_layers,
-            hidden_size=temporal_size,
-            bias = temporal_bias,
-        )
-        temporal_processor_type = str(temporal_processor_type).upper()
-        if temporal_processor_type == "NONE":
-            self.temporal_processor = nn.Identity()
-        elif temporal_processor_type == "LSTM":
-            self.temporal_processor = nn.LSTM(**rnn_kwargs)
-        elif temporal_processor_type == "GRU":
-            self.temporal_processor = nn.GRU(**rnn_kwargs)
-        else:
-            raise ValueError(f"Cannot recognize temporal processor type {temporal_processor_type}")
-
-        decoder_type = str(decoder_type).upper()
-        if decoder_type == "NONE":
-            self.decoder = None
-        elif decoder_type == "UNET":
-            self.decoder = UNetDecoder(in_channels=temporal_dims, out_channels=out_channels, initial=embedding_size, dropout=dropout)
-        else:
-            raise ValueError(f"Cannot recognize decoder type {decoder_type}")
-    
-    def forward(self, *inputs):
-        # Embedding
-        x = self.embedding(*inputs)
-
-        # Spatial Encoding
-        x, initial, l1, l2, l3, l4 = self.spatial_encoder(x)
-        
-        # Temporal processing
-        temporally_processed = self.temporal_processor(x)
-        if isinstance(temporally_processed, tuple):
-            temporally_processed = temporally_processed[0]
-                
-        l4 = l4 + temporally_processed.unsqueeze(dim=3).unsqueeze(dim=4).expand(l4.shape)
-
-        if self.decoder is None:
-            return temporally_processed
-
-        # Decoding
-        decoded = self.decoder(initial, l1, l2, l3, l4)
-
-        # Split into tuple
-        return split_tensor(decoded, self.output_types, feature_axis=2)
-
-
-class ThemedaModelUNetFastAI(nn.Module):
-    def __init__(
-        self,
-        input_types=List[PolyData],
-        output_types=List[PolyData],
-        embedding_size:int=16,        
-        encoder_resent:ResNet|str=ResNet.resnet18,
-        encoder_weights:str="DEFAULT",
-        temporal_processor_type:TemporalProcessorType|str=TemporalProcessorType.LSTM,
-        decoder_type:DecoderType|str=DecoderType.UNET,
-        temporal_layers:int=2,
-        temporal_size:int=512,
-        temporal_bias:bool=True,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.embedding = PolyEmbedding(
-            input_types=input_types,
-            embedding_size=embedding_size,
-            feature_axis=2,
-        )
-        self.output_types = output_types
-        out_channels = total_size(output_types)
-
-        self.unet = create_unet_model(
-            arch=resnet18, 
-            n_out=out_channels, 
-            img_size=(160,160), 
-            pretrained=True, 
-            cut=None, 
-            n_in=embedding_size
-        )
-
-    def forward(self, *inputs):
-        # Embedding
-        x = self.embedding(*inputs)
-
-        x, time_distributed, batch_size, timesteps = time_distributed_combine(x)
-
-        decoded = self.unet(x)
-
-        decoded = decoded.contiguous().view( (batch_size, timesteps, -1) + x.shape[2:] )  
-
-        # Split into tuple
-        return split_tensor(decoded, self.output_types, feature_axis=2)
 
 
 class ThemedaModel(nn.Module):
